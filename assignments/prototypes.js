b7ccca250813552eb5edc5717b7627cdc45baced
Heather Nuffer <zzzuzz@gmail.com>
2019-10-15 21:27:04 -0600

    Finished first part of prototypes section

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -1,11 +1,17 @@
 /*
-  Object oriented design is commonly used in video games.  For this part of the assignment you will be implementing several constructor functions with their correct inheritance hierarchy.
+  Object oriented design is commonly used in video games.  For this part of 
+  the assignment you will be implementing several constructor functions 
+  with their correct inheritance hierarchy.
 
-  In this file you will be creating three constructor functions: GameObject, CharacterStats, Humanoid.  
+  In this file you will be creating three constructor functions: 
+  GameObject, CharacterStats, Humanoid.  
 
-  At the bottom of this file are 3 objects that all end up inheriting from Humanoid.  Use the objects at the bottom of the page to test your constructor functions.
+  At the bottom of this file are 3 objects that all end up inheriting from 
+  Humanoid.  Use the objects at the bottom of the page to test your 
+  constructor functions.
   
-  Each constructor function has unique properties and methods that are defined in their block comments below:
+  Each constructor function has unique properties and methods that 
+  are defined in their block comments below:
 */
   
 /*
@@ -13,26 +19,58 @@
   * createdAt
   * name
   * dimensions (These represent the character's size in the video game)
-  * destroy() // prototype method that returns: `${this.name} was removed from the game.`
+  * destroy() // prototype method that returns: `${this.name} was removed 
+  * from the game.`
 */
+function GameObject(gameObjAttrs) {
+  this.createdAt = gameObjAttrs.createdAt;
+  this.name = gameObjAttrs.name;
+  this.dimensions = gameObjAttrs.dimensions;
+}
+GameObject.prototype.destroy = function() {
+  return `${this.name} was removed from the game.`;
+}
 
 /*
   === CharacterStats ===
   * healthPoints
-  * takeDamage() // prototype method -> returns the string '<object name> took damage.'
+  * takeDamage() // prototype method -> returns the string '<object name> 
+  * took damage.'
   * should inherit destroy() from GameObject's prototype
 */
+
+function CharacterStats(charStatsAttrs) {
+  GameObject.call(this, charStatsAttrs)
+  this.healthPoints = charStatsAttrs.healthPoints;
+}
+
+CharacterStats.prototype = Object.create(GameObject.prototype);
+CharacterStats.prototype.takeDamage = function() {
+  return `${this.name} took damage.`;
+}
 
 /*
   === Humanoid (Having an appearance or character resembling that of a human.) ===
   * team
   * weapons
   * language
-  * greet() // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
+  * greet() // prototype method -> returns the string '<object name> offers a 
+  * greeting in <object language>.'
   * should inherit destroy() from GameObject through CharacterStats
   * should inherit takeDamage() from CharacterStats
 */
- 
+ function Humanoid(humanoidAttrs) {
+   CharacterStats.call(this, humanoidAttrs);
+   this.team = humanoidAttrs.team;
+   this.weapons = humanoidAttrs.weapons;
+   this.language = humanoidAttrs.language;
+ }
+
+ Humanoid.prototype = Object.create(CharacterStats.prototype);
+ Humanoid.prototype.greet = function() {
+   return `${this.name} offers a greeting in ${this.language}.`;
+ }
+
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
@@ -41,7 +79,7 @@
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
-/*
+
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -102,7 +140,7 @@
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
+
 
   // Stretch task: 
   // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
